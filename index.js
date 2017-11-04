a9dfdf448529a78d2ade0890e6503829ab88faf3
Jennifer Haddock <jentx123@hotmail.com>
2017-11-04 03:20:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function changeLeastFavoriteCustomer() {
 
 function attemptTwoFavoriteCustomers() {
   const favoriteCustomer = 'big tipper';
-  let favoriteCustomer = 'nice lady';
+  const favoriteCustomer = 'nice lady';
 }
